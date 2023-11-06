b02a394cccfed862edf3ab0ed3323e48c22006a2
BravoClassic <gerald.akorli@gmail.com>
2023-11-06 11:00:31 -0700

    add logic to  fullURL for https://

--- src/App.jsx
+++ src/App.jsx
@@ -41,7 +41,7 @@ function App() {
     let response_type = "json";
     let fail_on_status = "400%2C404%2C500-511";
     let url_starter = "https://";
-    let fullURL = url_starter + inputs.url;
+    let fullURL =  inputs.url.includes(url_starter) ?  inputs.url : url_starter + inputs.url;
     let query = `https://api.apiflash.com/v1/urltoimage?access_key=${ACCESS_KEY}&url=${fullURL}&format=${inputs.format}&width=${inputs.width}&height=${inputs.height}&no_cookie_banners=${inputs.no_cookie_banners}&no_ads=${inputs.no_ads}&wait_until=${wait_until}&response_type=${response_type}&fail_on_status=${fail_on_status}`;
     callAPI(query).catch(console.error);
   };
